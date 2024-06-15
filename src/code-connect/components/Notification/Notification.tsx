69bfbd030f8f7104b840db9351fa7a8b934f33f6
Will Scott <78674552+sct-will@users.noreply.github.com>
2024-06-14 19:07:27 -0500

    Add files via upload

---
+++ src/code-connect/components/Notification/Notification.tsx
@@ -0,0 +1,9 @@
+import { InlineNotification } from "@carbon/react"
+
+// Disclaimer: Code Connect is currently in beta and integration with Carbon
+// React is in an exploratory phase. Code sample below may be incomplete.
+<InlineNotification
+    title="Title"
+    kind="info"
+    subtitle="Message"
+/>

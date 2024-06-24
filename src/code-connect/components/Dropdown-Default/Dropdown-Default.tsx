ebc9ffe4420faf1f655542ee96b966711473886c
Will Scott <78674552+sct-will@users.noreply.github.com>
2024-06-24 09:52:52 -0500

    Add files via upload

---
+++ src/code-connect/components/Dropdown-Default/Dropdown-Default.tsx
@@ -0,0 +1,22 @@
+import { Dropdown } from "@carbon/react";
+
+
+<Dropdown
+  id="id"
+  size="lg"
+  titleText="Label"
+  helperText="Helper text"
+  label="Choose an option"
+  warnText="Warning message goes here"
+  invalidText="Error message goes here"
+  items={[
+    {
+    id: "option-0",
+    text: "Option 0",
+    },
+    {
+    id: "option-1",
+    text: "Option 1",
+    },
+    ]}
+/>
